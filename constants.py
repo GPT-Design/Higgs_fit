b5c6f765f465e0c4f6ee8f18abd9bad578e18d41
GPTDesign <leo.germoni@gmail.com>
2025-07-28 23:13:48 +0930

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+# version 0.2
+"""
+Shared constants for the Higgs entropy fitting pipeline.
+"""
+
+# Binning and scaling defaults
+DEFAULT_BINS = 10
+DEFAULT_SCALE = 200_000.0
+
+# Physics constants
+DEFAULT_HIGGS_MASS = 125.0  # GeV
+DEFAULT_HIGGS_WIDTH = 0.004  # GeV
+DEFAULT_NORMALIZATION = 1.0
